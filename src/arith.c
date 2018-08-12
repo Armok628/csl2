0525a24ac6635be65ff4bcd2f678eff0dad20c6f
Mark Johnson <Mark.Johnson.62899@gmail.com>
2018-08-12 16:24:32 -0500

    Added other math operators

--- src/arith.c
+++ src/arith.c
@@ -2,20 +2,15 @@
 void init_arith(void)
 {
 	INIT(+,add);
+	INIT(-,subtract);
+	INIT(*,multiply);
+	INIT(/,divide);
 }
 static inline double dub(obj_t *n)
 {
 	return n->type==INTEGER?(double)n->data.i:n->data.d;
 }
-STACK(add,2_ARGS)
-obj_t *add(obj_t *x,obj_t *y)
-{
-	if (!type_check(x,INTEGER|DOUBLE,"+, arg 1: "))
-		return new_object();
-	if (!type_check(y,INTEGER|DOUBLE,"+, arg 2: "))
-		return new_object();
-	if (x->type==INTEGER&&y->type==INTEGER)
-		return new_integer(x->data.i+y->data.i);
-	double a=dub(x),b=dub(y);
-	return new_double(a+b);
-}
+FUNC_FROM_OP(+,add)
+FUNC_FROM_OP(-,subtract)
+FUNC_FROM_OP(*,multiply)
+FUNC_FROM_OP(/,divide)
--- src/arith.h
+++ src/arith.h
@@ -2,7 +2,23 @@
 #define ARITH_H
 #include "core.h"
 #include "namespace.h"
+#define FUNC_FROM_OP(op,name) \
+STACK(name,2_ARGS) \
+obj_t *name(obj_t *x,obj_t *y) \
+{ \
+	if (!type_check(x,INTEGER|DOUBLE,#op", arg 1: ")) \
+		return new_object(); \
+	if (!type_check(y,INTEGER|DOUBLE,#op", arg 2: ")) \
+		return new_object(); \
+	if (x->type==INTEGER&&y->type==INTEGER) \
+		return new_integer(x->data.i op y->data.i); \
+	double a=dub(x),b=dub(y); \
+	return new_double(a op b); \
+}
 
 void init_arith(void);
 CORE(add,obj_t *,obj_t *)
+CORE(subtract,obj_t *,obj_t *)
+CORE(multiply,obj_t *,obj_t *)
+CORE(divide,obj_t *,obj_t *)
 #endif
