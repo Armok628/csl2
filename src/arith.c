54d33eb3fc7bfbfe4ace424a0e6891c502e4e924
Mark Johnson <Mark.Johnson.62899@gmail.com>
2018-08-24 19:18:19 -0500

    Moved arith macros to .c file

--- src/arith.c
+++ src/arith.c
@@ -3,17 +3,6 @@ static inline double dub(obj_t *n)
 {
 	return n->type==INTEGER?(double)n->data.i:n->data.d;
 }
-FUNC_FROM_MATHOP(+,add)
-FUNC_FROM_MATHOP(-,subtract)
-FUNC_FROM_MATHOP(*,multiply)
-FUNC_FROM_MATHOP(/,divide)
-FUNC_FROM_COMP(>,gt)
-FUNC_FROM_COMP(>=,gte)
-FUNC_FROM_COMP(<,lt)
-FUNC_FROM_COMP(>=,lte)
-FUNC_FROM_COMP(==,eqn)
-FUNC_FROM_BOOLOP(||,or)
-FUNC_FROM_BOOLOP(&&,and)
 obj_t *l_random(obj_t *n)
 {
 	if (!type_check(n,DOUBLE|INTEGER,"RANDOM: "))
@@ -28,8 +17,77 @@ obj_t *l_random(obj_t *n)
 	}
 }
 STACK(l_random,1)
+#define FUNC_FROM_MATHOP(op,name) \
+obj_t *name(obj_t *x,obj_t *y) \
+{ \
+	bool type_err=false; \
+	type_err|=!type_check(x,INTEGER|DOUBLE,#op", arg 1: "); \
+	type_err|=!type_check(y,INTEGER|DOUBLE,#op", arg 2: "); \
+	if (type_err) \
+		return new_object(); \
+	if (x->type==INTEGER&&y->type==INTEGER) \
+		return new_integer(x->data.i op y->data.i); \
+	double a=dub(x),b=dub(y); \
+	return new_double(a op b); \
+} \
+STACK(name,2)
+FUNC_FROM_MATHOP(+,add)
+FUNC_FROM_MATHOP(-,subtract)
+FUNC_FROM_MATHOP(*,multiply)
+FUNC_FROM_MATHOP(/,divide)
+#define FUNC_FROM_COMP(op,name) \
+obj_t *name(obj_t *x,obj_t *y) \
+{ \
+	bool type_err=false; \
+	type_err|=!type_check(x,INTEGER|DOUBLE,#op", arg 1: "); \
+	type_err|=!type_check(y,INTEGER|DOUBLE,#op", arg 2: "); \
+	if (type_err) \
+		return new_object(); \
+	double a=dub(x),b=dub(y); \
+	if (a op b) \
+		return strsym("T"); \
+	else \
+		return NULL; \
+} \
+STACK(name,2)
+FUNC_FROM_COMP(>,gt)
+FUNC_FROM_COMP(>=,gte)
+FUNC_FROM_COMP(<,lt)
+FUNC_FROM_COMP(>=,lte)
+FUNC_FROM_COMP(==,eqn)
+#define FUNC_FROM_BOOLOP(op,name) \
+obj_t *name(obj_t *x,obj_t *y) \
+{ \
+	if (x op y) \
+		return strsym("T"); \
+	else \
+		return NULL; \
+} \
+STACK(name,2)
+FUNC_FROM_BOOLOP(||,or)
+FUNC_FROM_BOOLOP(&&,and)
+#define FUNC_FROM_MATHFUNC_2(func) \
+obj_t *l_##func(obj_t *x,obj_t *y) \
+{ \
+	double a=dub(x),b=dub(y); \
+	double d=func(a,b); \
+	if (fmod(d,1.0)<1e-6) \
+		return new_integer((long)d); \
+	return new_double(d); \
+} \
+STACK(l_##func,2)
 FUNC_FROM_MATHFUNC_2(pow)
 FUNC_FROM_MATHFUNC_2(fmod)
+#define FUNC_FROM_MATHFUNC_1(func) \
+obj_t *l_##func(obj_t *x) \
+{ \
+	double d=dub(x); \
+	d=func(d); \
+	if (fmod(d,1.0)<1e-6) \
+		return new_integer((long)d); \
+	return new_double(d); \
+} \
+STACK(l_##func,1)
 FUNC_FROM_MATHFUNC_1(floor)
 FUNC_FROM_MATHFUNC_1(ceil)
 FUNC_FROM_MATHFUNC_1(round)
@@ -37,6 +95,11 @@ FUNC_FROM_MATHFUNC_1(trunc)
 FUNC_FROM_MATHFUNC_1(sqrt)
 FUNC_FROM_MATHFUNC_1(exp)
 FUNC_FROM_MATHFUNC_1(log)
+#define FUNCS_FROM_TRIG(func) \
+FUNC_FROM_MATHFUNC_1(func) \
+FUNC_FROM_MATHFUNC_1(func##h) \
+FUNC_FROM_MATHFUNC_1(a##func) \
+FUNC_FROM_MATHFUNC_1(a##func##h)
 FUNCS_FROM_TRIG(sin)
 FUNCS_FROM_TRIG(cos)
 FUNCS_FROM_TRIG(tan)
