3484323a0a93629b1105941b2e43d62156a2c5db
Mark Johnson <Mark.Johnson.62899@gmail.com>
2018-09-25 15:19:21 -0500

    Make multable test more confusing

--- tests/multable.lisp
+++ tests/multable.lisp
@@ -1,7 +1,5 @@
 #!/usr/local/bin/ispl
 (progn
   (source lib)
-  (set 'multiplier (lambda '(n) '(lambda '(m) `(* ,n m))))
-  (set 'table_row (lambda '(n) '(mapcar (multiplier n) (range 1 1 10))))
-  (foreach 'row (mapcar table_row (range 1 1 10)) '(progn (foreach 'n row '(print n \t)) (terpri)))
+  (foreach 'row (mapcar (lambda '(n) '(mapcar (lambda '(m) `(* ,n m)) (range 1 1 10))) (range 1 1 10)) '(progn (foreach 'n row '(print n \t)) (terpri)))
   )
