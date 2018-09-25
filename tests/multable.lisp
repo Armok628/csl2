76df0788c082c6a098a0592636543ea51defb8b9
Mark Johnson <Mark.Johnson.62899@gmail.com>
2018-09-25 15:28:51 -0500

    Make multable test less confusing

--- tests/multable.lisp
+++ tests/multable.lisp
@@ -2,5 +2,8 @@
 (progn
   (source lib)
   (set 'max (if (> (length argv) 2) (nth 2 argv) 12))
-  (foreach 'row (mapcar (lambda '(n) '(mapcar (lambda '(m) `(* ,n m)) (range 1 1 max))) (range 1 1 max)) '(progn (foreach 'n row '(print n \t)) (terpri)))
+  (set 'multiplier (lambda '(n) '(lambda '(m) `(* ,n m))))
+  (set 'tablerow (lambda '(n) '(mapcar (multiplier n) (range 1 1 max))))
+  (foreach 'row (mapcar tablerow (range 1 1 max))
+	   '(progn (foreach 'n row '(print n \t)) (terpri)))
   )
