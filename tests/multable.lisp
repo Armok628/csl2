b0eece9af89fef7daad49eff5839860ab9e3f487
Mark Johnson <Mark.Johnson.62899@gmail.com>
2018-09-25 15:22:58 -0500

    Make multable test even more confusing

--- tests/multable.lisp
+++ tests/multable.lisp
@@ -1,5 +1,6 @@
 #!/usr/local/bin/ispl
 (progn
   (source lib)
-  (foreach 'row (mapcar (lambda '(n) '(mapcar (lambda '(m) `(* ,n m)) (range 1 1 10))) (range 1 1 10)) '(progn (foreach 'n row '(print n \t)) (terpri)))
+  (set 'max (if (> (length argv) 2) (nth 2 argv) 12))
+  (foreach 'row (mapcar (lambda '(n) '(mapcar (lambda '(m) `(* ,n m)) (range 1 1 max))) (range 1 1 max)) '(progn (foreach 'n row '(print n \t)) (terpri)))
   )
