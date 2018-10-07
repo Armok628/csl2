8d42551a562f66ef4e64d97f1a8c26a6f311e375
Mark Johnson <Mark.Johnson.62899@gmail.com>
2018-10-07 00:54:06 -0500

    Vastly improved truth table demo

--- demos/collatz.lisp
+++ demos/collatz.lisp
@@ -3,6 +3,8 @@
   (set 'collatz (lambda '(n) '(if (== n 1) '(1) (cons n (recurse (if (== (mod n 2) 0) (/ n 2) (+ (* 3 n) 1)))))))
   (set 'n (read))
   (if (not (eq (typeof n) 'INTEGER)) (set 'n 670617279))
+  (tick)
   (set 'c (collatz n))
+  (print (tock) \n)
   (print c \n (length c) \n)
   )
--- demos/truth.lisp
+++ demos/truth.lisp
@@ -1,8 +1,10 @@
 #!/usr/local/bin/ispl
 (progn
-  (set 'expr (read))
-  (foreach 'a '(nil t)
- '(foreach 'b '(nil t)
- '(foreach 'c '(nil t)
- '(foreach 'd '(nil t)
-	   '(print a \t b \t c \t d \t \t (eval expr) \n))))))
+  (set 'printer (lambda '(s) '(if s `(,(car s) \t @(recurse (cdr s))) `(\t ,(inside dictionary 'expr) \n))))
+  (set 'loopbody (lambda '(s o) '(if s `(foreach (quote ,(car s)) '(nil t) (quote ,(recurse (cdr s) o))) o)))
+  (print '"Variables: ") (set 'vars (read))
+  (print '"Expression: ") (set 'expr (read))
+  (foreach 'var vars '(print var \t))
+  (terpri)
+  (eval (loopbody vars (cons 'print (printer vars))))
+  )
